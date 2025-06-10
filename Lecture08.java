2696c1684926acf606f4fb993495427a61628b07
GordonZhang20231004 <baoshuaizhang8@gmail.com>
2025-06-10 15:58:36 +0900

    再帰呼び出しの授業での練習

---
+++ Lecture08.java
@@ -0,0 +1,17 @@
+//再帰呼び出しの授業での練習
+public class Lecture08 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            System.out.println("f(" + i + ")" + f(i));
+        }
+    }
+
+    static long f(int n) {
+        if (n < 2) {
+            return 1;
+        } else {
+            return n * f(n - 1);
+        }
+
+    }
+}
