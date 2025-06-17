1cc55b7999ad7f840d499ba2adf20cd62cfc941f
GordonZhang20231004 <baoshuaizhang8@gmail.com>
2025-06-17 15:48:21 +0900

    第8回コード

--- Lecture09/PointTest.java
+++ Lecture09/PointTest.java
@@ -4,6 +4,7 @@ public class PointTest {
     public static void main(String[] args) {
 
         Point p = new Point(2, -3);
+
         System.out.println("p: " + p);
         System.out.println("p.getX(): " + p.getX());
         System.out.println("p.getY(): " + p.getY());
